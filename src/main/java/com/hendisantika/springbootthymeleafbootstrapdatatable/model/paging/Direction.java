1dec7c37352911921166ce517e8d6e2ebddd76dd
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-26 13:38:44 +0700

    #10 - Add Direction Enum Entity

---
+++ src/main/java/com/hendisantika/springbootthymeleafbootstrapdatatable/model/paging/Direction.java
@@ -0,0 +1,15 @@
+package com.hendisantika.springbootthymeleafbootstrapdatatable.model.paging;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-thymeleaf-bootstrap-datatable
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 26/11/20
+ * Time: 13.38
+ */
+public enum Direction {
+    asc,
+    desc;
+}
