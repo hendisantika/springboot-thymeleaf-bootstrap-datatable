b19fce5240144f0d6170991ce94ff56ae1a812d0
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-26 13:05:43 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootthymeleafbootstrapdatatable/SpringbootThymeleafBootstrapDatatableApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootthymeleafbootstrapdatatable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeleafBootstrapDatatableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootThymeleafBootstrapDatatableApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootthymeleafbootstrapdatatable/SpringbootThymeleafBootstrapDatatableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootthymeleafbootstrapdatatable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootThymeleafBootstrapDatatableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
