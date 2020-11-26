097b52fbd37e8b812c1775708c23839dbfd8c5b7
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-26 13:44:15 +0700

    #17 - Add getPage service in EmployeeService

--- src/main/java/com/hendisantika/springbootthymeleafbootstrapdatatable/service/EmployeeService.java
+++ src/main/java/com/hendisantika/springbootthymeleafbootstrapdatatable/service/EmployeeService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -43,6 +44,26 @@ public class EmployeeService {
         }
 
         return new Page<>();
+    }
+
+    private Page<Employee> getPage(List<Employee> employees, PagingRequest pagingRequest) {
+        List<Employee> filtered = employees.stream()
+                .sorted(sortEmployees(pagingRequest))
+                .filter(filterEmployees(pagingRequest))
+                .skip(pagingRequest.getStart())
+                .limit(pagingRequest.getLength())
+                .collect(Collectors.toList());
+
+        long count = employees.stream()
+                .filter(filterEmployees(pagingRequest))
+                .count();
+
+        Page<Employee> page = new Page<>(filtered);
+        page.setRecordsFiltered((int) count);
+        page.setRecordsTotal((int) count);
+        page.setDraw(pagingRequest.getDraw());
+
+        return page;
     }
 
 }
