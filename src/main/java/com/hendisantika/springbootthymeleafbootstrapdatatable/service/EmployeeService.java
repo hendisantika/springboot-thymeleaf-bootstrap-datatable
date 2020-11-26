395ff24d839841088792d6a90d59bd4c1fb19be9
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-26 13:43:46 +0700

    #16 - Add getEmployees service in EmployeeService

--- src/main/java/com/hendisantika/springbootthymeleafbootstrapdatatable/service/EmployeeService.java
+++ src/main/java/com/hendisantika/springbootthymeleafbootstrapdatatable/service/EmployeeService.java
@@ -1,10 +1,16 @@
 package com.hendisantika.springbootthymeleafbootstrapdatatable.service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hendisantika.springbootthymeleafbootstrapdatatable.model.Employee;
+import com.hendisantika.springbootthymeleafbootstrapdatatable.model.paging.Page;
+import com.hendisantika.springbootthymeleafbootstrapdatatable.model.paging.PagingRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.Comparator;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,5 +26,23 @@ import java.util.Comparator;
 public class EmployeeService {
 
     private static final Comparator<Employee> EMPTY_COMPARATOR = (e1, e2) -> 0;
+
+    public Page<Employee> getEmployees(PagingRequest pagingRequest) {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            List<Employee> employees = objectMapper.readValue(getClass().getClassLoader()
+                            .getResourceAsStream("employees.json"),
+                    new TypeReference<List<Employee>>() {
+                    });
+
+            return getPage(employees, pagingRequest);
+
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
+
+        return new Page<>();
+    }
 
 }
